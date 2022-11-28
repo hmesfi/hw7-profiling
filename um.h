75be724d272345f3624d1ef1b8e8cdf40c3cd1ce
hmesfi <107152570+hmesfi@users.noreply.github.com>
2022-11-28 16:40:42 -0500

    Create um.h

---
+++ um.h
@@ -0,0 +1,32 @@
+/********************************************************************
+ *
+ *                     um.h
+ *
+ *     Assignment: um
+ *     Authors:  Dan Patterson (dpatte04), Helina Mesfin (hmesfi01)
+ *     Date:     Nov 21, 2022
+ *
+ *     Purpose:
+ *      
+ *     
+ *     Interface for um.h module. Contains functions
+ *     that run the UM.
+ ********************************************************************/
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdint.h>
+#include <bitpack.h>
+#include <seq.h>
+#include "memory_segment.h"
+
+#ifndef UM
+#define UM
+
+typedef struct um_T um_T;
+
+/* Performs all the functions of the UM. */
+void run_um(Seq_T instructions); 
+
+#endif
